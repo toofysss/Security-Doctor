8c1a9c0914a17a5bfc7a5990591e0c0c930e80ab
toofysss <148949998+toofysss@users.noreply.github.com>
2024-07-14 23:02:50 +0300

    7/14

---
+++ Api/SecurityDoctor/SecurityDoctor/Modles/FilesManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecurityDoctor.Modles
+{
+    public class FilesManager
+    {
+
+        public static async Task<string> Insertfiles(IFormFile File,string path)
+        {
+            var extension = "." + File.FileName.Split('.')[^1];
+            string Filename = DateTime.Now.Ticks.ToString() + extension;
+             var filepath = Path.Combine(Directory.GetCurrentDirectory(), path);
+
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+
+            var exactpath = Path.Combine(Directory.GetCurrentDirectory(), path, Filename);
+            using (var stream = new FileStream(exactpath, FileMode.Create))
+            {
+                await File.CopyToAsync(stream);
+            }
+
+
+            return Filename;
+        }
+
+        public static string RemoveFile(string oldImage,string path)
+        {
+
+            var extension = "." + oldImage.Split('.')[^1];
+            string filename = DateTime.Now.Ticks.ToString() + extension;
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), path);
+ 
+            if (System.IO.File.Exists(Path.Combine(filePath, oldImage)))
+            {
+                System.IO.File.Delete(Path.Combine(filePath, oldImage));
+            }
+            return filename;
+
+        }
+    }
+}
---
+++ Api/SecurityDoctor/SecurityDoctor/Modles/SendEmail.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecurityDoctor.Modles
+{
+    public class SendEmail
+    {
+        private static readonly Random random = new Random();
+        private const string chars = "0123456789";
+
+        private static string GenerateRandomInt(int length)
+        {
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        public static  string SendEmailMessage(string Email)
+        {
+        
+                string authCode = GenerateRandomInt(6);
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse("fa2535373@gmail.com"));
+                email.To.Add(MailboxAddress.Parse(Email));
+                email.Subject = "رمز التحقق";
+                email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = "رمز التحقق الخاص بك هو :" + authCode
+                };
+                using var smtp = new SmtpClient();
+                smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                smtp.Authenticate("fa2535373@gmail.com", "oeefbkrtklbfeata");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+                return authCode;
+  
+        }
+
+      
+    }
+}
